7b9c9528230a2c539324d0caaeeb033dca3c8872
Subham-CodeX <stanix535@gmail.com>
2026-07-02 23:00:14 +0530

    convert into PWA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { registerSW }
+from "virtual:pwa-register";
+
 import "./index.css";
 import App from "./App.jsx";
 
-import { TaskProvider } from "./context/TaskContext";
+import { TaskProvider }
+from "./context/TaskContext";
 
-createRoot(document.getElementById("root")).render(
+registerSW({
+
+  immediate: true,
+
+});
+
+createRoot(
+document.getElementById("root")
+).render(
+
   <StrictMode>
+
     <TaskProvider>
+
       <App />
+
     </TaskProvider>
+
   </StrictMode>
+
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,85 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+
+  plugins: [
+
+    react(),
+
+    VitePWA({
+
+      registerType: "autoUpdate",
+
+      injectRegister: "auto",
+
+      devOptions: {
+
+        enabled: true,
+
+      },
+
+      includeAssets: [
+
+        "favicon.svg",
+
+        "apple-touch-icon.png",
+
+        "maskable-icon-512.png",
+
+      ],
+
+      manifest: {
+
+        id: "/",
+
+        name: "TaskFlow",
+
+        short_name: "TaskFlow",
+
+        description:
+          "Modern Todo App built with React",
+
+        theme_color: "#6F42F5",
+
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        orientation: "portrait",
+
+        start_url: "/",
+
+        scope: "/",
+
+        icons: [
+
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+
+          {
+            src: "maskable-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          }
+
+        ]
+
+      }
+
+    })
+
+  ]
+
+});
